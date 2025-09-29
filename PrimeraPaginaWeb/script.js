ca5449e0cc43ba760c05c7069697546440a4f872
Javier Zazo Morillo <javiezaz@ucm.es>
2025-09-29 14:29:12 +0200

    macarrones

--- PrimeraPaginaWeb/script.js
+++ PrimeraPaginaWeb/script.js
@@ -2,7 +2,7 @@ function devuelveTextoDeAlerta() {
   return "uooooo! Vaya alerta";
 }
 
-function desaparece(clase) {
+function desapareceClase(clase) {
   let elementos = document.getElementsByClassName(clase);
 
     for (let i = 0; i < elementos.length; i++) {
@@ -10,26 +10,37 @@ function desaparece(clase) {
     }
 }
 
+function desaparece(id) {
+  let elemento = document.getElementById(id);
+  elemento.style.visibility='hidden';
+}
+
 function reemplazarPorVideo(clase) {
-  var elementos = document.getElementsByClassName(clase);
-  var padre = elementos[0].parentNode;
+  let elementos = document.getElementsByClassName(clase);
+  let padre = elementos[0].parentNode;
+  const componentes = [];
 
   for (let i = 0; i < elementos.length; i++) {
-    var video = document.createElement('video');
+    componentes[i] = (document.createElement('li'));
+
+    const video = document.createElement('video');
     video.setAttribute('src', 'assets/fade-away-oooooooooooo.mp4');
     video.setAttribute('class', "emoji");
     video.setAttribute('width', 50);
     video.setAttribute('autoplay', true);
     video.setAttribute('muted', true);
-    //video.setAttribute('onpause = "desaparece(video.getAttribute("class"))"', true);
-    video.onpause = desaparece("emoji");
+    video.onpause = "componentes[i].remove()";
 
-    var componente = document.createElement('li');
-    componente.appendChild(video);
+    componentes[i].appendChild(video);
     
-    var elementoAntiguo = elementos[i];
-    padre.replaceChild(video, elementos[i]);
+    let elementoAntiguo = elementos[i];
+    padre.replaceChild(componentes[i], elementos[i]);
     padre.appendChild(elementoAntiguo);
   }
-  //desaparece(clase);
+  desapareceClase(clase);
 }
+ 
+function que(cosa) {
+  console.log(cosa);
+  console.log(typeof(cosa));
+  }
