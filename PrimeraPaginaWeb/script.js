2c001cf3f21dae25547d6c3a954c4f9f06e65845
link10804k <62154757+link10804k@users.noreply.github.com>
2025-09-29 18:50:21 +0200

    😢

--- PrimeraPaginaWeb/script.js
+++ PrimeraPaginaWeb/script.js
@@ -1,46 +1,29 @@
-function devuelveTextoDeAlerta() {
-  return "uooooo! Vaya alerta";
-}
+function AlternarVisibilidad(clase) {
+  let elementosClase = document.getElementsByClassName(clase);
 
-function desapareceClase(clase) {
-  let elementos = document.getElementsByClassName(clase);
-
-    for (let i = 0; i < elementos.length; i++) {
-      elementos[i].style.visibility='hidden';  
+  if (elementosClase[0].style.visibility=='hidden') {
+    for (let i = 0; i < elementosClase.length; i++) {
+      elementosClase[i].style.visibility='visible'
     }
+  }
+  else {
+    for (let i = 0; i < elementosClase.length; i++) {
+      elementosClase[i].style.visibility='hidden'
+    }
+  }
 }
 
-function desaparece(id) {
-  let elemento = document.getElementById(id);
-  elemento.style.visibility='hidden';
-}
-
-function reemplazarPorVideo(clase) {
-  let elementos = document.getElementsByClassName(clase);
-  let padre = elementos[0].parentNode;
-  const componentes = [];
+function AlternarColor() {
+  let lista = document.getElementById("lista");
+  let elementos = lista.children;
+  console.log(elementos.item(0));
 
   for (let i = 0; i < elementos.length; i++) {
-    componentes[i] = (document.createElement('li'));
-
-    const video = document.createElement('video');
-    video.setAttribute('src', 'assets/fade-away-oooooooooooo.mp4');
-    video.setAttribute('class', "emoji");
-    video.setAttribute('width', 50);
-    video.setAttribute('autoplay', true);
-    video.setAttribute('muted', true);
-    video.onpause = "componentes[i].remove()";
-
-    componentes[i].appendChild(video);
-    
-    let elementoAntiguo = elementos[i];
-    padre.replaceChild(componentes[i], elementos[i]);
-    padre.appendChild(elementoAntiguo);
+    if (elementos.item(i).className == "like") {
+      elementos.item(i).style.color = "green";
+    }
+    else {
+      elementos.item(i).style.color = "red";
+    }
   }
-  desapareceClase(clase);
 }
- 
-function que(cosa) {
-  console.log(cosa);
-  console.log(typeof(cosa));
-  }
