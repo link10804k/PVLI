ae3cb540636852cc40ab6f5e274d6e7a93183a44
Javier Zazo Morillo <javiezaz@ucm.es>
2025-09-30 19:58:35 +0200

    e

--- PrimeraPaginaWeb/script.js
+++ PrimeraPaginaWeb/script.js
@@ -1,29 +1,44 @@
 function AlternarVisibilidad(clase) {
   let elementosClase = document.getElementsByClassName(clase);
 
-  if (elementosClase[0].style.visibility=='hidden') {
+  if (elementosClase[0].firstChild.style.display=='none' || elementosClase[0].style.visibility=='hidden' || elementosClase[0].style.visibility=='') {
     for (let i = 0; i < elementosClase.length; i++) {
-      elementosClase[i].style.visibility='visible'
+      elementosClase[i].firstChild.style.display='inline';
     }
   }
   else {
     for (let i = 0; i < elementosClase.length; i++) {
-      elementosClase[i].style.visibility='hidden'
+      elementosClase[i].firstChild.style.display='inline';
+      VideoPlay(elementosClase[i].children[0]);
     }
+  }
+}
+
+function VideoPlay(video) {
+  video.style.visibility='visible';
+  video.play();
+  video.onended = function() {
+    video.style.visibility='hidden';
   }
 }
 
 function AlternarColor() {
   let lista = document.getElementById("lista");
   let elementos = lista.children;
-  console.log(elementos.item(0));
 
-  for (let i = 0; i < elementos.length; i++) {
-    if (elementos.item(i).className == "like") {
-      elementos.item(i).style.color = "green";
+  if (elementos[0].style.color=='black') {
+    for (let i = 0; i < elementos.length; i++) {
+      if (elementos[i].className == "like") {
+        elementos[i].style.color = "green";
+      }
+      else {
+        elementos[i].style.color = "red";
+      }
     }
-    else {
-      elementos.item(i).style.color = "red";
+  }
+  else {
+    for (let i = 0; i < elementos.length; i++) {
+      elementos[i].style.color = "black";
     }
   }
 }
